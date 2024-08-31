f86fe5bca644d954c340db83da101cdba0b33387
Pedro Páez <pedro_ppc@hotmail.com>
2024-08-31 03:33:15 -0500

    bug fix

--- components/TestimonialSection.tsx
+++ components/TestimonialSection.tsx
@@ -5,47 +5,60 @@ import EmblaCarousel from './EmblaCarousel'
 import Slider from './Slider'
 import CarouselComponent from './CarouselComponent'
 
-
 const TestimonialSection = () => {
-
     const OPTIONS: EmblaOptionsType = { loop: true }
-    const SLIDE_COUNT = 3
-    const SLIDES = [
-        <TestimonialCard
-            testimonial='Me encantó el viaje! Recomendado! Me encantó el viaje! Recomendado!'
-            touristName='Mark'
-            travelDestination="Brasil"
-            tripDate='02/2024'
-            imageUrl='/client1.jpg'
-            imageAlt='Testimonio1'
-            ratingImageUrl='/ratings5.png'
-        />,
-        <TestimonialCard
-            testimonial='Me encantó el viaje! Recomendado! Me encantó el viaje! Recomendado!'
-            touristName='Mark'
-            travelDestination="Brasil"
-            tripDate='02/2024'
-            imageUrl='/client2.jpg'
-            imageAlt='Testimonio1'
-            ratingImageUrl='/ratings5.png'
-        />,
-        <TestimonialCard
-            testimonial='Me encantó el viaje! Recomendado! Me encantó el viaje! Recomendado!'
-            touristName='Mark'
-            travelDestination="Brasil"
-            tripDate='02/2024'
-            imageUrl='/client3.jpg'
-            imageAlt='Testimonio1'
-            ratingImageUrl='/ratings5.png'
-        />,
-      ]
+    
+    // Array of testimonial data
+    const testimonials = [
+        {
+            testimonial: 'Me encantó el viaje! Recomendado! Me encantó el viaje! Recomendado!',
+            touristName: 'Mark',
+            travelDestination: 'Brasil',
+            tripDate: '02/2024',
+            imageUrl: '/client1.jpg',
+            imageAlt: 'Testimonio1',
+            ratingImageUrl: '/ratings5.png'
+        },
+        {
+            testimonial: 'Me encantó el viaje! Recomendado! Me encantó el viaje! Recomendado!',
+            touristName: 'Mark',
+            travelDestination: 'Brasil',
+            tripDate: '02/2024',
+            imageUrl: '/client2.jpg',
+            imageAlt: 'Testimonio2',
+            ratingImageUrl: '/ratings5.png'
+        },
+        {
+            testimonial: 'Me encantó el viaje! Recomendado! Me encantó el viaje! Recomendado!',
+            touristName: 'Mark',
+            travelDestination: 'Brasil',
+            tripDate: '02/2024',
+            imageUrl: '/client3.jpg',
+            imageAlt: 'Testimonio3',
+            ratingImageUrl: '/ratings5.png'
+        }
+    ]
 
-  return (
-    <div className='flex items-center px-12 mt-24'>
-        {/* <EmblaCarousel slides={SLIDES} options={OPTIONS}/> */}
-        <CarouselComponent/>
-    </div>
-  )
+    // Generate slides using map
+    const SLIDES = testimonials.map((testimonial, index) => (
+        <TestimonialCard
+            key={`testimonial-${index}`} // Unique key for each TestimonialCard
+            testimonial={testimonial.testimonial}
+            touristName={testimonial.touristName}
+            travelDestination={testimonial.travelDestination}
+            tripDate={testimonial.tripDate}
+            imageUrl={testimonial.imageUrl}
+            imageAlt={testimonial.imageAlt}
+            ratingImageUrl={testimonial.ratingImageUrl}
+        />
+    ))
+
+    return (
+        <div className='flex items-center px-12 mt-24'>
+            {/* <EmblaCarousel slides={SLIDES} options={OPTIONS}/> */}
+            <CarouselComponent />
+        </div>
+    )
 }
 
 export default TestimonialSection
